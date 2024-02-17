999f485148534f937bb3b326e2bd340433c95f6b
unknown <dudnikovaanna545@gmail.com>
2024-02-17 21:30:14 +0300

    ok i admit it`s harder

---
+++ perehod.py
@@ -0,0 +1,31 @@
+import unittest
+import math
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+class MyPerehod(unittest.TestCase):
+    def test_perehod(self):
+        from selenium import webdriver
+        from selenium.webdriver.common.by import By
+        import time
+
+        link = "http://suninjuly.github.io/redirect_accept.html"
+        browser = webdriver.Chrome()
+        browser.get(link)
+        button = browser.find_element(By.TAG_NAME, "button")
+        button.click()
+        new_window = browser.window_handles[1]
+        browser.switch_to.window(new_window)
+        x_element = browser.find_element(By.ID, "input_value")
+        x = x_element.text
+        y = calc(x)
+        input1 = browser.find_element(By.ID, "answer")
+        input1.send_keys(y)
+        button = browser.find_element(By.CSS_SELECTOR, "[type='submit']")
+        button.click()
+        time.sleep(3)
+
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ treasure.py
@@ -0,0 +1,50 @@
+import unittest
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+class MyTreasure(unittest.TestCase):
+    def test_treasure(self):
+        from selenium import webdriver
+        from selenium.webdriver.common.by import By
+        import time
+
+        try:
+
+            link = "http://suninjuly.github.io/get_attribute.html"
+            browser = webdriver.Chrome()
+            browser.get(link)
+            x_element = browser.find_element(By.ID, "treasure")
+            x = people_checked = x_element.get_attribute("valuex")
+            y = calc(x)
+
+            # Ваш код, который заполняет обязательные поля
+            input1 = browser.find_element(By.ID, "answer")
+            input1.send_keys(y)
+            option1 = browser.find_element(By.ID, "robotCheckbox")
+            option1.click()
+            option2 = browser.find_element(By.ID, "robotsRule")
+            option2.click()
+
+            # time.sleep(3)
+
+            # Отправляем заполненную форму
+            button = browser.find_element(By.CSS_SELECTOR, "[type='submit']")
+            button.click()
+
+            # Проверяем, что смогли зарегистрироваться
+            # ждем загрузки страницы
+            time.sleep(1)
+
+
+
+        finally:
+            # ожидание чтобы визуально оценить результаты прохождения скрипта
+            time.sleep(5)
+            # закрываем браузер после всех манипуляций
+            browser.quit()
+
+
+if __name__ == '__main__':
+    unittest.main()
