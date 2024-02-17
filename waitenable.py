85e08e451862682d42de3cda5f41b806553d7d90
unknown <dudnikovaanna545@gmail.com>
2024-02-17 21:41:08 +0300

    the last wan also added

--- waitenable.py
+++ waitenable.py
@@ -10,7 +10,6 @@ class MyEnable(unittest.TestCase):
 
         browser.get("http://suninjuly.github.io/wait2.html")
 
-        # говорим Selenium проверять в течение 5 секунд, пока кнопка не станет кликабельной
         button = WebDriverWait(browser, 5).until(
             EC.element_to_be_clickable((By.ID, "verify"))
         )
